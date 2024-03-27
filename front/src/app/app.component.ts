6ebcc9d160358b049d34e65aa77023c70639d356
Yoannmont <y.mont@orange.fr>
2024-03-27 16:59:03 +0100

    fixed footer

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, LoginPageComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
